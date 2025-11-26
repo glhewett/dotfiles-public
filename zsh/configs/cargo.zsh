1641a4bb262fefc8da442b5a02274e63453e6a61
Greg Hewett <glh@strand3.com>
2025-11-26 18:09:13 +0000

    .local

--- zsh/configs/cargo.zsh
+++ zsh/configs/cargo.zsh
@@ -1,2 +1,2 @@
-export CARGO_INSTALL_ROOT=$HOME/local
-
+export CARGO_INSTALL_ROOT=$HOME/.local
+export PATH=$PATH:$HOME/.local/bin
--- zsh/configs/self-compiled.zsh
+++ zsh/configs/self-compiled.zsh
@@ -1,4 +1,3 @@
-export PATH=${HOME}/local/bin:$PATH
-export MANPATH=${HOME}/local/share/man:$MANPATH
-export PKG_CONFIG_PATH=${HOME}/local/lib/pkgconfig:$HOME/local/share/pkgconfig
-export LD_LIBRARY_PATH=${HOME}/local/lib
+export MANPATH=${HOME}/.local/share/man:$MANPATH
+export PKG_CONFIG_PATH=${HOME}/.local/lib/pkgconfig:$HOME/local/share/pkgconfig
+export LD_LIBRARY_PATH=${HOME}/.local/lib
